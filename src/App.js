bb913672ba85fc2f761633803025d9ca35c1f3e7
raspanda <carlviberg@gmail.com>
2021-05-05 21:47:06 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './styles/style.scss'
 
 const App = () => {
-  return <h1>Carl Viberg Portfolio</h1>
+  return <h1>Carl Viberg - Portfolio</h1>
 }
 
-export default App
+export default Appß
